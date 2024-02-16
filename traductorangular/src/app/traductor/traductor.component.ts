2153fa17ab50a6699c27254aaf4ed4a13da19aab
jocarsa <info@josevicentecarratala.com>
2024-02-16 11:45:31 +0100

    resultado

--- traductorangular/src/app/traductor/traductor.component.ts
+++ traductorangular/src/app/traductor/traductor.component.ts
@@ -4,44 +4,36 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-traductor',
   templateUrl: './traductor.component.html',
-  styleUrl: './traductor.component.css'
+  styleUrls: ['./traductor.component.css']
 })
 export class TraductorComponent {
     constructor(private http: HttpClient){}
     misdatos: any;
+
     ngOnInit(){
-        this.http.get('assets/traductor.json').subscribe(data => {
-        
+        this.http.get<any[]>('assets/traductor.json').subscribe(data => {
             this.misdatos = data;
-            setTimeout(function(){
-                let filas = document.getElementsByClassName("fila")
-                
-                console.log(filas.length)
-                for(let i = 0;i<filas.length;i++){
+            setTimeout(() => {
+                let filas = document.getElementsByClassName("fila");
+                console.log(filas.length);
+                for(let i = 0; i < filas.length; i++){
                     (filas[i] as HTMLElement).style.display = "none";
-                    
-            }
-            },1000)
-            
-        })
-        
-        console.log("hola")
+                }
+            }, 1000);
+        });
+        console.log("hola");
     }
+
     enviar() {
         const contenido = (document.getElementById('termino') as HTMLInputElement).value;
         console.log(contenido);
-        console.log("Ahora lo comparo con mis datos:")
-        this.http.get('assets/traductor.json').subscribe(data => {
-        console.log("ok hola")
-            let jsonData = JSON.parse(JSON.stringify(data))
-            console.log(jsonData)
-            for(let i = 0;i<jsonData.length;i++){
-                
-                if(jsonData[i]['es'] == contenido){
-                    console.log("coincidencia encontrada")
-                    console.log(jsonData[i])
-                }
+        console.log("Ahora lo comparo con mis datos:");
+        for (let i = 0; i < this.misdatos.length; i++) {
+            if (this.misdatos[i]['es'] === contenido) {
+                console.log("coincidencia encontrada");
+                console.log(this.misdatos[i]);
+                (document.getElementById(contenido) as HTMLElement).style.display = "table-row";
             }
-        })
+        }
     }
 }
